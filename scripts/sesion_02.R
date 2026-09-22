fe860bb91f88cc6d27b61b4b4b49442f7a887bc5
scidatmath2020 <56654363+scidatmath2020@users.noreply.github.com>
2026-09-21 18:39:57 -0600

    Add files via upload

---
+++ scripts/sesion_02.R
@@ -0,0 +1,40 @@
+mi_primer_vector_numerico = c(1,-4,2026)
+mi_primer_vector_logico = c(TRUE,TRUE,FALSE,TRUE)
+
+no_vector = c(5,"hola")
+
+mi_primer_lista = list(5,TRUE,"hola a todos")
+
+lista_con_lista = list(FALSE,
+                       list("A",5),
+                       c(5,8,10)
+                       )
+2+6
+# HOola a todos. Esta es una muy buena sesión de nuestro
+# diplomado
+5*8
+
+###### R distingue entre mayúsculas y minúsculas
+###### Es decir, no es lo mismo el texto "hola" que "Hola"
+
+### Ejemplo
+
+Vector1 = c(2,7)
+vector1 = c(5,8)
+
+vector_singulete <- c(TRUE)
+
+c("hola a todos") -> vector_contrario
+
+
+
+
+
+
+
+
+
+
+
+
+
